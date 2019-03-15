46b05c6f2af8766a3f571319fa619a9330697745
vliubchenko <v.liubchenko@pinta.com.ua>
2019-03-15 17:59:01 +0200

    3 variat

--- catalog/controller/extension/module/simple.php
+++ catalog/controller/extension/module/simple.php
@@ -1,16 +1,16 @@
 <?php
+
 class ControllerExtensionModuleSimple extends Controller
 {
     private $setting = array();
+    private $min_symbol;
     private $search;
-    public function searchs( )
-    {
-        if(!empty($this->request->post['search'])){
-           $this->search =  trim(strip_tags(stripcslashes(htmlspecialchars($this->request->post['search']))));
 
-        }
+    public function searchs()
+    {
+
         $this->load->model('extension/module/simple');
-        if(empty($this->setting)) {
+        if (empty($this->setting)) {
             $this->setting = $this->model_extension_module_simple->getSetting();
             foreach ($this->setting as $key => $item) {
 
@@ -23,8 +23,12 @@ class ControllerExtensionModuleSimple extends Controller
                 }
             }
         }
+        if (!empty($this->request->post['search'])) {
+            $this->search = trim(strip_tags(stripcslashes(htmlspecialchars($this->request->post['search']))));
+
+        }
         $result = array();
-        if($this->setting['setting_input']['setting_status'] == 1){
+        if ($this->setting['setting_input']['setting_status'] == 1) {
             $setting = $this->setting['setting_input'];
             $category = $this->setting['category_input'];
             $information = $this->setting['information_input'];
@@ -32,36 +36,36 @@ class ControllerExtensionModuleSimple extends Controller
             $product = $this->setting['product_input'];
             $desing = $this->setting['desing_input'];
             $query = array();
-           foreach ($category as $name=>$val) {
-               if ($val == 1) {
-                   $query[] = str_replace('category_', '', $name);
-               }
-           }
-           if(!empty($query)){
+            if (mb_strlen($this->search) >= $setting['setting_min_symbol']) {
+                foreach ($category as $name => $val) {
+                    if ($val == 1) {
+                        $query[] = str_replace('category_', '', $name);
+                    }
+                }
+                if (!empty($query)) {
 
-                 $result['category'] = $this->model_extension_module_simple->getCategory($this->search,$query);
-                $results = '';
-           foreach ($result['category'] as $item){
-               echo '<li><a href="http://open3.test/index.php?route=product/category&path='.$item['category_id'].'">'.$item['name'].'</a></li>';
-           }
-           }
-            foreach ($product as $name=>$val) {
-                if ($val == 1) {
-                    $product_query[] = str_replace('product_', '', $name);
+                    $result['category'] = $this->model_extension_module_simple->getCategory($this->search, $query);
+
+                    foreach ($result['category'] as $item) {
+                        echo '<li><a href="http://open3.test/index.php?route=product/category&path=' . $item['category_id'] . '">' . $item['name'] . '</a></li>';
+                    }
+                }
+                foreach ($product as $name => $val) {
+                    if ($val == 1) {
+                        $product_query[] = str_replace('product_', '', $name);
+                    }
+                }
+                if (!empty($product_query)) {
+
+
+                    $result['product'] = $this->model_extension_module_simple->getProduct($this->search, $product_query);
+
+                    foreach ($result['product'] as $item) {
+                        echo '<li><a href="http://open3.test/index.php?route=product/product&path='
+                            . $item['category_id'] . '&product_id=' . $item['product_id'] . '" >' . $item['name'] . '</a></li>';
+                    }
                 }
             }
-            if(!empty($product_query)){
-
-
-               $result['product'] = $this->model_extension_module_simple->getProduct($this->search,$product_query);
-
-              foreach ($result['product'] as $item){
-                  echo '<li><a href="http://open3.test/index.php?route=product/product&path='.$item['category_id'].'&product_id='.$item['product_id'].'" >'.$item['name'].'</a></li>';
-              }
-            }
-
-        } else {
-            return false;
         }
 
     }
